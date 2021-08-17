2c1d6b3d3267f50c8a93043911154dbd6c791ab1
sheilaLygreWeitling <U2407194@rts-365.dk>
2021-08-17 14:20:40 +0200

    add assignment 01b

--- eks_01b_DOMogEvents/js/index.js
+++ eks_01b_DOMogEvents/js/index.js
@@ -1,6 +1,43 @@
+
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
+
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
-
-}); // Afslutter: DOMContentLoaded
+	let startNumber = 5; /*deklærer number variablen*/
+	let buttonPlus = document.querySelector("#btn-count-increase");
+	let buttonMinus = document.querySelector("#btn-count-decrease");
+	let textCountElement = document.querySelector("#text-count");
+	let minimunNumber = 0;
+	let maximumNumber = 10;
+	
+	function updateNumber () {	
+		textCountElement.textContent = startNumber;
+	} /*functionen der opdatere nummeret, hvis den ikke kaldes, så "virker" den ikke*/
+	
+	updateNumber(); /*kalder function*/
+	
+	buttonPlus.addEventListener("click", (e) => { 
+		/* => en anden form for function. tilføjer en eventlistener til 
+		buttonPlus*/
+		if(startNumber < maximumNumber){ 
+			/*hvis number, som er deklæreret til at være 5, er mindre end 10, så læg en 1 til. 
+			*/
+			startNumber++;
+			updateNumber();
+			/*opdater number*/
+		}
+	});
+	
+	buttonMinus.addEventListener("click", (e) => { 
+		/* => en anden form for function. tilføjer en eventlistener til 
+		buttonMinus*/
+		if(startNumber > minimunNumber){
+			/*hvis number, som er deklæreret til at være 5, er større end 0, så læg en 1 til. 
+			*/
+			startNumber--;
+			updateNumber();
+			/*opdater number*/
+		}
+	});
+	
+	}); // Afslutter: DOMContentLoaded
