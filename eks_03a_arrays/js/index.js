5742fa920e73a77bf42bcc9226d851aaec54489c
sheilaLygreWeitling <U2407194@rts-365.dk>
2021-08-19 15:06:13 +0200

    add solution to assignment 03a

--- eks_03a_arrays/js/index.js
+++ eks_03a_arrays/js/index.js
@@ -1,6 +1,31 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+	const names = [
+		"Anders", 
+		"Fedtmule", 
+		"Mickey", 
+		"Rip", 
+		"Rap", 
+		"Rup"];
+
+	const nameContainer = document.querySelector("#liste"); 
+	const unorderedListOfNames = document.createElement("ul");
+
+	unorderedListOfNames.setAttribute("style", "padding: 0; margin: 0;");
+	unorderedListOfNames.setAttribute("id", "nameContainer");
+	
+	unorderedListOfNames.setAttribute("names", "nameContainer");
+	for(i = 0; i <= names.length - 1; i++){
+
+		const listOfNames = document.createElement("li");
+
+		listOfNames.innerHTML = names[i];
+		listOfNames.setAttribute("style", "display", "block");
+
+		unorderedListOfNames.appendChild(listOfNames);
+	}	
+
+	nameContainer.appendChild(unorderedListOfNames);
 
 }); // Afslutter: DOMContentLoaded
